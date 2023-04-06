21d9dcc45ed75dc756887c86e60486a2efd27042
sumanthyarajala <S556122@nwmissouri.edu>
2023-04-05 21:29:33 -0500

    added selector.js & pug files

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eraserRouter = require('./routes/eraser');
 var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
 
 
 var app = express();
@@ -26,6 +27,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/eraser', eraserRouter);
 app.use('/board', boardRouter);
+app.use('/selector', selectorRouter);
 
 
 // catch 404 and forward to error handler
