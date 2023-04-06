a9201e22cd33c154e9e69bc0c068ff4324ab7864
sumanthyarajala <S556122@nwmissouri.edu>
2023-04-05 21:08:36 -0500

    add board.js & pug files

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eraserRouter = require('./routes/eraser');
+var boardRouter = require('./routes/board');
 
 
 var app = express();
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/eraser', eraserRouter);
+app.use('/board', boardRouter);
 
 
 // catch 404 and forward to error handler
